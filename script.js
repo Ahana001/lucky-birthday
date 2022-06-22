8a8836553eb10301c387737e3ad4e7b466c6dc27
Ahana001 <>
2022-06-22 17:21:47 +0530

    initial commit

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ const number = document.querySelector("#lucky-number");
 const result = document.querySelector("#output");
 let sum = 0;
 
-function calculateSum (){
+function calculateSum() {
   let date = dob.value;
-  date = date.replaceAll("-","");
+  date = date.replaceAll("-", "");
   //console.log(typeof date);
   let dateArray = date.split("");
-  dateArray.map(( val )=>{
+  dateArray.map((val) => {
     sum = sum + parseInt(val);
   });
-  //console.log(sum,number);
+
 }
-function calculateLuckyBirth(){
+function calculateLuckyBirth() {
   //console.log(typeof number);
-  if(sum % number.value == 0){
+  if (sum % number.value == 0) {
     result.innerText = "Your birthday is lucky 🚀";
-  }else{
+  } else {
     result.innerText = "Your birthday is not lucky 🤒";
   }
 }
-button.addEventListener("click",()=>{
+button.addEventListener("click", () => {
   calculateSum();
   calculateLuckyBirth();
 });
